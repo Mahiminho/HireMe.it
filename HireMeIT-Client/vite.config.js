a8f138852251dc50dbdd47b4f701489b93bfb740
Maxim Popov <maximpopovdev@gmail.com>
2025-04-03 10:09:50 +0300

    Commit final?

--- HireMeIT-Client/vite.config.js
+++ HireMeIT-Client/vite.config.js
@@ -1,3 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       }
+//     },
+//     headers: {
+//       "Content-Type": "application/javascript",
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,11 +25,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: process.env.BACKEND_URL || 'https://hireme-it.onrender.com',
         changeOrigin: true,
+        secure: false
       }
     },
     headers: {
