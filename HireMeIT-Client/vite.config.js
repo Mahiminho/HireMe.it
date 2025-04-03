d0feb29c7a04812ab3959cfecb6be95ae862bad6
Maxim Popov <maximpopovdev@gmail.com>
2025-04-03 18:09:58 +0300

    Commited fix file

--- HireMeIT-Client/vite.config.js
+++ HireMeIT-Client/vite.config.js
@@ -18,7 +18,6 @@
 //   }
 // })
 
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
---
+++ build.sh
@@ -0,0 +1,6 @@
+cd ./HireMeIT-Client
+npm install
+npm run build
+cd ..
+cd ./HireMeIT-Server
+pip install -r requirements.txt
